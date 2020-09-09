1e2e29e3e0e4e039125b9db5c2a0eef580fcf3b2
whiteononon <whiteon0618@gmail.com>
2020-09-09 21:14:47 +0900

    create event form and table with bootstrap

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <div className="container-fluid">
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">タイトル</label>
+          <input className="form-control" id="formEventTitle" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="formEventBody">ボディー</label>
+          <textarea className="form-control" id="formEventBody" />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          イベントを作成する
+        </button>
+        <button type="submit" className="btn btn-danger">
+          全てのイベントを削除する
+        </button>
+      </form>
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">タイトル</th>
+            <th scope="col">ボディー</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
